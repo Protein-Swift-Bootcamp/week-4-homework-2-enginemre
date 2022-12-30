d6940874c69d42c79e9b69822b6f2faec98ed141
enginemre <engiinemre@gmail.com>
2022-12-30 11:29:36 +0300

    Initial Commit

---
+++ homework-two/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework-two
+//
+//  Created by Emre Muhammet Engin on 30.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
